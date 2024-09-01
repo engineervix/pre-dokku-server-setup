75b2a4b5ef2bf64073f86217a49238e77d8e9741
Victor Miti <victormiti@umusebo.com>
2024-09-01 16:08:50 +0100

    chore: we need unattended upgrades & fail2ban notifications

--- setup.sh
+++ setup.sh
@@ -100,7 +100,7 @@ function main() {
         setupMailwithMailJet
         configureUpdatesNotificationsAndLogwatch
     fi
-    # configureUnattendedUpgrades
+    configureUnattendedUpgrades
     furtherHardening
     miscellaneousTasks
     installExtraPackages
@@ -428,9 +428,9 @@ function furtherHardening() {
     sudo apt install fail2ban -y
     sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
     echo -e "\e[35m===========================================================\e[00m"
-    # echo -e "\e[35mNow updating the /etc/fail2ban/jail.local file ...\e[00m"
-    # sudo sed -i "s/^destemail\ =\ root@localhost/destemail\ =\ $root_email/" /etc/fail2ban/jail.local
-    # sudo sed -i "s/^sender\ =\ root@<fq-hostname>/sender\ =\ $mail_from/" /etc/fail2ban/jail.local
+    echo -e "\e[35mNow updating the /etc/fail2ban/jail.local file ...\e[00m"
+    sudo sed -i "s/^destemail\ =\ root@localhost/destemail\ =\ $root_email/" /etc/fail2ban/jail.local
+    sudo sed -i "s/^sender\ =\ root@<fq-hostname>/sender\ =\ $mail_from/" /etc/fail2ban/jail.local
     echo -e "\e[35menabling sshd ...\e[00m"
     sudo sed -i '/^\[sshd\]/a enabled\ =\ true' /etc/fail2ban/jail.local
     echo -e "\e[35m===========================================================\e[00m"
